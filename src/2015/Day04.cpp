09329755611033b7db254c12bffa5c0f058e638c
Liam Metzger <liam.metzger@blkb.ch>
2024-07-19 11:00:34 +0200

    Solve 2015-04

--- src/2015/Day04.cpp
+++ src/2015/Day04.cpp
@@ -1,13 +1,32 @@
 #include "2015/Day04.hpp"
 
+#include "coll-aoc-runner/crypto/MD5.hpp"
+
+
 Day04_2015::Day04_2015() {
-  exampleInput = "";
+  exampleInput = "abcdef";
 }
 
 string Day04_2015::part1(const string& input, bool example) {
-  return "TODO: Solve part 1.";
+  int i = 1;
+
+  while (i > 0) {
+    string hash = caoc::crypto::md5(input + to_string(i));
+    if (hash.starts_with("00000")) break;
+    i++;
+  }
+
+  return to_string(i);
 }
 
 string Day04_2015::part2(const string& input, bool example) {
-  return "TODO: Solve part 2.";
+  int i = 1;
+
+  while (i > 0) {
+    string hash = caoc::crypto::md5(input + to_string(i));
+    if (hash.starts_with("000000")) break;
+    i++;
+  }
+
+  return to_string(i);
 }
