ce20b1f9295fecaa44a5f13e25f0c278f9f9eec0
Liam Metzger <liam.metzger@blkb.ch>
2024-07-19 09:36:01 +0200

    Solve 2015-03

--- src/2015/Day03.cpp
+++ src/2015/Day03.cpp
@@ -1,13 +1,47 @@
 #include "2015/Day03.hpp"
 
+#include <set>
+#include <utility>
+
 Day03_2015::Day03_2015() {
-  exampleInput = "";
+  exampleInput = "^>v<";
 }
 
 string Day03_2015::part1(const string& input, bool example) {
-  return "TODO: Solve part 1.";
+  int x = 0, y = 0;
+  set<pair<int, int>> houses {{x, y}};
+
+  for (auto c : input) {
+    switch (c) {
+      case '^': y++; break;
+      case 'v': y--; break;
+      case '>': x++; break;
+      case '<': x--; break;
+    }
+    houses.insert({x, y});
+  }
+
+  return to_string(houses.size());
 }
 
 string Day03_2015::part2(const string& input, bool example) {
-  return "TODO: Solve part 2.";
+  int santaX = 0, santaY = 0;
+  int roboX = 0, roboY = 0;
+  set<pair<int, int>> houses {{santaX, santaY}};
+
+  for (int i = 0; i < input.size(); i++) {
+    int& x = i % 2 == 0 ? santaX : roboX;
+    int& y = i % 2 == 0 ? santaY : roboY;
+
+    switch (input[i]) {
+      case '^': y++; break;
+      case 'v': y--; break;
+      case '>': x++; break;
+      case '<': x--; break;
+    }
+
+    houses.insert({x, y});
+  }
+
+  return to_string(houses.size());
 }
