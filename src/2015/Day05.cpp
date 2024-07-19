da22b714d7d449039cf60d00d52d0ca90a2d4be8
Liam Metzger <liam.metzger@blkb.ch>
2024-07-19 12:03:49 +0200

    Solve 2015-05

--- src/2015/Day05.cpp
+++ src/2015/Day05.cpp
@@ -1,13 +1,30 @@
 #include "2015/Day05.hpp"
 
+#include <regex>
+#include <sstream>
+
 Day05_2015::Day05_2015() {
-  exampleInput = "";
+  exampleInput = "ugknbfddgicrmopn\naaa\njchzalrnumimnmhp\nhaegwjzuvuyypxyu\ndvszwmarrgswjxmb";
 }
 
 string Day05_2015::part1(const string& input, bool example) {
-  return "TODO: Solve part 1.";
+  int count = 0;
+
+  stringstream stream(input); string line;
+  while (getline(stream, line, '\n')) {
+    if (regex_search(line, regex(R"(^(?=.*(\w)\1)(?=.*([aeiou].*){3,})(?!.*?ab|.*?cd|.*?pq|.*?xy).*$)"))) count++;
+  }
+
+  return to_string(count);
 }
 
 string Day05_2015::part2(const string& input, bool example) {
-  return "TODO: Solve part 2.";
+  int count = 0;
+
+  stringstream stream(input); string line;
+  while (getline(stream, line, '\n')) {
+    if (regex_search(line, regex(R"(^(?=.*(\w\w).*\1)(?=.*(\w).\2.*).*$)"))) count++;
+  }
+
+  return to_string(count);
 }
