5fa383e30ca0271c598a13a60e1ae922da40eb1e
Liam Metzger <liam.metzger@blkb.ch>
2024-08-07 09:32:20 +0200

    Solve 2015-11

--- src/2015/Day11.cpp
+++ src/2015/Day11.cpp
@@ -1,13 +1,60 @@
 #include "2015/Day11.hpp"
 
 Day11_2015::Day11_2015() {
-  exampleInput = "";
+  exampleInput = "abcdefgh";
+}
+
+bool validPassword(const string &password) {
+  bool rule1 = false;
+
+  int pairs = 0;
+  bool skip = false;
+
+  for (int i = 0; i + 1 < password.size(); i++) {
+    if (rule1 && pairs >= 2) return true;
+    if (rule1 == false && i + 2 < password.size()) {
+      if (password.at(i) + 1 == password.at(i + 1) && password.at(i) + 2 == password.at(i + 2))
+        rule1 = true;
+    }
+
+    if (skip) { skip = false; continue; }
+    if (password.at(i) == password.at(i + 1)) {
+      pairs++;
+
+      if (rule1 == true) i++;
+      else skip = true;
+    }
+  }
+
+  return rule1 && pairs >= 2;
+}
+
+string incrementPassword(const string& password) {
+  string incrementedPassword = password;
+
+  do {
+    for (int i = incrementedPassword.size() - 1; i >= 0; i--) {
+      if (incrementedPassword[i] == 'z') {
+        incrementedPassword[i] = 'a';
+      } else {
+        incrementedPassword[i]++;
+
+        // Rule 2
+        if (incrementedPassword[i] == 'i' || incrementedPassword[i] == 'o' || incrementedPassword[i] == 'l')
+          incrementedPassword[i]++;
+
+        break;
+      }
+    }
+  } while (!validPassword(incrementedPassword));
+
+  return incrementedPassword;
 }
 
 string Day11_2015::part1(const string& input, bool example) {
-  return "TODO: Solve part 1.";
+  return incrementPassword(input);
 }
 
 string Day11_2015::part2(const string& input, bool example) {
-  return "TODO: Solve part 2.";
+  return incrementPassword(incrementPassword(input));
 }
