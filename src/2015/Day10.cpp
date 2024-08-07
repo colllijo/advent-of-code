8168a5af46abe7a63c53a161595a031803900940
Liam Metzger <liam.metzger@blkb.ch>
2024-08-07 08:22:19 +0200

    Solve 2015-10

--- src/2015/Day10.cpp
+++ src/2015/Day10.cpp
@@ -1,13 +1,42 @@
 #include "2015/Day10.hpp"
+#include <cwchar>
 
 Day10_2015::Day10_2015() {
   exampleInput = "";
 }
 
+string lookAndSay(const string &str) {
+  string result;
+
+  char instance;
+  int count = 0;
+
+  for (const auto &c : str) {
+    if (c != instance) {
+      if (count != 0) result += to_string(count) + instance;
+      instance = c; count = 1;
+    } else {
+      count++;
+    }
+  }
+
+  return result + to_string(count) + instance;
+}
+
 string Day10_2015::part1(const string& input, bool example) {
-  return "TODO: Solve part 1.";
+  string result = input;
+
+  for (int i = 0; i < 40; i++)
+    result = lookAndSay(result);
+
+  return to_string(result.size());
 }
 
 string Day10_2015::part2(const string& input, bool example) {
-  return "TODO: Solve part 2.";
+  string result = input;
+
+  for (int i = 0; i < 50; i++)
+    result = lookAndSay(result);
+
+  return to_string(result.size());
 }
