2336758aecdae03beb86484dc86d6abc74f72ee0
Liam Metzger <liam.metzer@bluewin.ch>
2024-11-15 13:37:40 +0100

    Solve 2015-23

--- src/2015/Day23.cpp
+++ src/2015/Day23.cpp
@@ -1,13 +1,134 @@
 #include "2015/Day23.hpp"
 
-Day23_2015::Day23_2015() {
-  exampleInput = "";
+#include <map>
+#include <regex>
+#include <sstream>
+#include <vector>
+
+Day23_2015::Day23_2015()
+{
+	exampleInput =
+	    "inc a\n"
+	    "jio a, +2\n"
+	    "tpl a\n"
+	    "inc a\n";
 }
 
-string Day23_2015::part1(const string& input, bool example) {
-  return "TODO: Solve part 1.";
+struct Instruction
+{
+	string command;
+	string registerName;
+	int offset;
+};
+
+string Day23_2015::part1(const string& input, bool example)
+{
+	int instructionOffset = 0;
+	vector<Instruction> instructions;
+	map<string, unsigned long> regs{{"a", 0}, {"b", 0}};
+
+	stringstream stream(input);
+	string line;
+	while (getline(stream, line, '\n'))
+	{
+		Instruction instruction{"", "", 0};
+		smatch match;
+
+		if (regex_search(line, match, regex("^(jie|jio) (\\w+), ([-+]?\\d+)$")))
+		{
+			instruction.command = match[1];
+			instruction.registerName = match[2];
+			instruction.offset = stoi(match[3]);
+		}
+		else if (regex_search(line, match, regex("^(jmp) ([-+]?\\d+)$")))
+		{
+			instruction.command = match[1];
+			instruction.offset = stoi(match[2]);
+		}
+		else if (regex_search(line, match, regex("^(hlf|tpl|inc) (\\w+)$")))
+		{
+			instruction.command = match[1];
+			instruction.registerName = match[2];
+		}
+
+		instructions.push_back(instruction);
+	}
+
+	while (instructionOffset < instructions.size())
+	{
+		auto [cmd, reg, off] = instructions[instructionOffset];
+
+		if (cmd == "hlf")
+			regs[reg] /= 2;
+		else if (cmd == "tpl")
+			regs[reg] *= 3;
+		else if (cmd == "inc")
+			regs[reg]++;
+		else if (cmd == "jmp")
+			instructionOffset += off - 1;
+		else if (cmd == "jie" && (regs[reg] & 0b1) == 0)
+			instructionOffset += off - 1;
+		else if (cmd == "jio" && regs[reg] == 1)
+			instructionOffset += off - 1;
+
+		instructionOffset++;
+	}
+
+	return to_string(regs["b"]);
 }
 
-string Day23_2015::part2(const string& input, bool example) {
-  return "TODO: Solve part 2.";
+string Day23_2015::part2(const string& input, bool example)
+{
+	int instructionOffset = 0;
+	vector<Instruction> instructions;
+	map<string, unsigned long> regs{{"a", 1}, {"b", 0}};
+
+	stringstream stream(input);
+	string line;
+	while (getline(stream, line, '\n'))
+	{
+		Instruction instruction{"", "", 0};
+		smatch match;
+
+		if (regex_search(line, match, regex("^(jie|jio) (\\w+), ([-+]?\\d+)$")))
+		{
+			instruction.command = match[1];
+			instruction.registerName = match[2];
+			instruction.offset = stoi(match[3]);
+		}
+		else if (regex_search(line, match, regex("^(jmp) ([-+]?\\d+)$")))
+		{
+			instruction.command = match[1];
+			instruction.offset = stoi(match[2]);
+		}
+		else if (regex_search(line, match, regex("^(hlf|tpl|inc) (\\w+)$")))
+		{
+			instruction.command = match[1];
+			instruction.registerName = match[2];
+		}
+
+		instructions.push_back(instruction);
+	}
+
+	while (instructionOffset < instructions.size())
+	{
+		auto [cmd, reg, off] = instructions[instructionOffset];
+
+		if (cmd == "hlf")
+			regs[reg] /= 2;
+		else if (cmd == "tpl")
+			regs[reg] *= 3;
+		else if (cmd == "inc")
+			regs[reg]++;
+		else if (cmd == "jmp")
+			instructionOffset += off - 1;
+		else if (cmd == "jie" && (regs[reg] & 0b1) == 0)
+			instructionOffset += off - 1;
+		else if (cmd == "jio" && regs[reg] == 1)
+			instructionOffset += off - 1;
+
+		instructionOffset++;
+	}
+
+	return to_string(regs["b"]);
 }
