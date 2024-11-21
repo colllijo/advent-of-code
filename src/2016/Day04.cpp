588d7abd500c5806cbabcb6ddba086c112a0f5c4
Liam Metzger <liam.metzer@bluewin.ch>
2024-11-21 09:37:19 +0100

    Solve 2016-04

--- src/2016/Day04.cpp
+++ src/2016/Day04.cpp
@@ -1,13 +1,106 @@
 #include "2016/Day04.hpp"
 
-Day04_2016::Day04_2016() {
-	exampleInput = "";
+#include <algorithm>
+#include <map>
+#include <regex>
+#include <sstream>
+
+Day04_2016::Day04_2016()
+{
+	exampleInput =
+	    "aaaaa-bbb-z-y-x-123[abxyz]\n"
+	    "a-b-c-d-e-f-g-h-987[abcde]\n"
+	    "not-a-real-room-404[oarel]\n"
+	    "totally-real-room-200[decoy]\n";
 }
 
-string Day04_2016::part1(const string& input, bool example) {
-	return "TODO: Solve part 1.";
+string calculateChecksum(string name)
+{
+	map<char, int> counts;
+	for (char c : name)
+	{
+		if (c == '-') continue;
+		counts[c]++;
+	}
+
+	vector<pair<char, int>> sorted;
+	for (const auto& pair : counts)
+	{
+		sorted.push_back(pair);
+	}
+
+	sort(sorted.begin(), sorted.end(),
+	     [](const pair<char, int>& a, const pair<char, int>& b)
+	     {
+		     if (a.second != b.second) return a.second > b.second;
+		     return a.first < b.first;
+	     });
+
+	string calculatedChecksum;
+	for (int i = 0; i < 5; i++)
+	{
+		calculatedChecksum += sorted[i].first;
+	}
+
+  return calculatedChecksum;
 }
 
-string Day04_2016::part2(const string& input, bool example) {
-	return "TODO: Solve part 2.";
+string Day04_2016::part1(const string& input, bool example)
+{
+	int sum = 0;
+
+	stringstream stream(input);
+	string line;
+	while (getline(stream, line))
+	{
+		smatch match;
+
+		if (regex_search(line, match, regex("([a-z-]+)-([0-9]+)\\[([a-z]+)\\]")))
+		{
+			string name = match[1];
+			int id = stoi(match[2]);
+			string checksum = match[3];
+
+			if (calculateChecksum(name) == checksum)
+			{
+				sum += id;
+			}
+		}
+	}
+
+	return to_string(sum);
+}
+
+string Day04_2016::part2(const string& input, bool example)
+{
+	stringstream stream(input);
+	string line;
+	while (getline(stream, line))
+	{
+		smatch match;
+
+		if (regex_search(line, match, regex("([a-z-]+)-([0-9]+)\\[([a-z]+)\\]")))
+		{
+			string name = match[1];
+			int id = stoi(match[2]);
+			string checksum = match[3];
+
+			if (calculateChecksum(name) == checksum)
+			{
+        for (char& c : name)
+        {
+          if (c == '-') { c = ' '; }
+          else
+          {
+            c = ((c - 'a' + id) % 26) + 'a';
+          }
+        }
+
+        if (name == "northpole object storage")
+        { return to_string(id); }
+			}
+		}
+	}
+
+	return "Storage not found";
 }
