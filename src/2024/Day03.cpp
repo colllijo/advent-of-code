b5794e4b4c0175a5ba1ea9fc7130b4a3733c6047
Liam Metzger <liam.metzger@bluewin.ch>
2024-12-03 06:12:27 +0100

    Solve 2024-03

--- src/2024/Day03.cpp
+++ src/2024/Day03.cpp
@@ -1,13 +1,71 @@
 #include "2024/Day03.hpp"
 
-Day03_2024::Day03_2024() {
-	exampleInput = "";
+#include <regex>
+
+Day03_2024::Day03_2024() { exampleInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"; }
+
+string Day03_2024::part1(const string& input, bool example)
+{
+	unsigned long result = 0;
+
+	regex pattern(R"((mul\((\d+),(\d+)\)))");
+	auto mBegin = sregex_iterator(input.begin(), input.end(), pattern);
+	auto mEnd = sregex_iterator();
+
+	if (mBegin == mEnd)
+	{
+		return "0";
+	}
+
+	for (sregex_iterator i = mBegin; i != mEnd; ++i)
+	{
+		smatch match = *i;
+
+		printf("Match: %s\n", match.str().c_str());
+
+		int left = stoi(match.str().substr(4, match.str().find(',') - 1));
+		int right = stoi(match.str().substr(match.str().find(',') + 1));
+
+		result += left * right;
+	}
+
+	return to_string(result);
 }
 
-string Day03_2024::part1(const string& input, bool example) {
-	return "TODO: Solve part 1.";
-}
+string Day03_2024::part2(const string& input, bool example)
+{
+	unsigned long result = 0;
+	bool enabled = true;
 
-string Day03_2024::part2(const string& input, bool example) {
-	return "TODO: Solve part 2.";
+	regex pattern(R"((mul\((\d+),(\d+)\)|do\(\)|don't\(\)))");
+	auto mBegin = sregex_iterator(input.begin(), input.end(), pattern);
+	auto mEnd = sregex_iterator();
+
+	if (mBegin == mEnd)
+	{
+		return "0";
+	}
+
+	for (sregex_iterator i = mBegin; i != mEnd; ++i)
+	{
+		smatch match = *i;
+
+		if (match.str().starts_with("do()"))
+		{
+			enabled = true;
+		}
+		else if (match.str().starts_with("don't()"))
+		{
+			enabled = false;
+		}
+		else if (match.str().starts_with("mul") && enabled)
+		{
+			int left = stoi(match.str().substr(4, match.str().find(',') - 1));
+			int right = stoi(match.str().substr(match.str().find(',') + 1));
+
+			result += left * right;
+		}
+	}
+
+	return to_string(result);
 }
