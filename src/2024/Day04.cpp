e1d8cbbae88a04fb795f2d06162e383af5e81c27
Liam Metzger <liam.metzer@bluewin.ch>
2024-12-05 11:14:39 +0100

    Update 2024-04 to use Direction

--- src/2024/Day04.cpp
+++ src/2024/Day04.cpp
@@ -1,8 +1,9 @@
 #include "2024/Day04.hpp"
 
-#include <sstream>
-#include <vector>
+#include <numeric>
+#include <string>
 
+#include "coll-aoc-runner/structs/Direction.hpp"
 #include "coll-aoc-runner/structs/Grid.hpp"
 
 Day04_2024::Day04_2024()
@@ -20,61 +21,32 @@ Day04_2024::Day04_2024()
 	    "MXMXAXMASX";
 }
 
-std::vector<std::vector<char>> rotate90Degrees(const std::vector<std::vector<char>>& matrix)
-{
-	int rows = matrix.size();
-	int cols = matrix[0].size();
-	std::vector<std::vector<char>> rotated(cols, std::vector<char>(rows));
-
-	for (int i = 0; i < rows; ++i)
-	{
-		for (int j = 0; j < cols; ++j)
-		{
-			rotated[j][i] = matrix[i][j];
-		}
-	}
-
-	return rotated;
-}
-
 string Day04_2024::part1(const string& input, bool example)
 {
+	string search = "XMAS";
 	int count = 0;
 	caoc::structs::Grid<char> grid(input);
 
-	for (const auto& row : grid.rows())
+	for (int y = 0; y < grid.height(); y++)
 	{
-		for (int i = 0; i < row.size() - 3; i++)
+		for (int x = 0; x < grid.width(); x++)
 		{
-			if ((row[i] == 'X' && row[i + 1] == 'M' && row[i + 2] == 'A' && row[i + 3] == 'S') || (row[i] == 'S' && row[i + 1] == 'A' && row[i + 2] == 'M' && row[i + 3] == 'X'))
+			for (const auto& dir : caoc::structs::Directions)
 			{
-				count++;
-			}
-		}
-	}
+				try
+				{
+					for (int i = 0; i < search.size(); i++)
+					{
+						if (grid.move(x, y, dir, i) != search[i]) goto next;
+					}
 
-	for (const auto& column : grid.columns())
-	{
-		for (int i = 0; i < column.size() - 3; i++)
-		{
-			if ((column[i] == 'X' && column[i + 1] == 'M' && column[i + 2] == 'A' && column[i + 3] == 'S') ||
-			    (column[i] == 'S' && column[i + 1] == 'A' && column[i + 2] == 'M' && column[i + 3] == 'X'))
-			{
-				count++;
-			}
-		}
-	}
+					count++;
+				}
+				catch (const std::out_of_range&)
+				{
+				}
 
-	for (const auto& diagonal : grid.diagonals())
-	{
-		if (diagonal.size() < 3) continue;
-
-		for (int i = 0; i < diagonal.size() - 3; i++)
-		{
-			if ((diagonal[i] == 'X' && diagonal[i + 1] == 'M' && diagonal[i + 2] == 'A' && diagonal[i + 3] == 'S') ||
-			    (diagonal[i] == 'S' && diagonal[i + 1] == 'A' && diagonal[i + 2] == 'M' && diagonal[i + 3] == 'X'))
-			{
-				count++;
+			next:
 			}
 		}
 	}
@@ -85,22 +57,23 @@ string Day04_2024::part1(const string& input, bool example)
 string Day04_2024::part2(const string& input, bool example)
 {
 	long long count = 0;
-  caoc::structs::Grid<char> grid(input);
+	caoc::structs::Grid<char> grid(input);
 
-	for (int y = 0; y < grid.height() - 2; y++)
+	auto crossCount = [&grid](int x, int y)
 	{
-		for (int x = 0; x < grid.width() - 2; x++)
+		using caoc::structs::Direction;
+		std::vector<Direction> diagonals = {Direction::NORTH_EAST, Direction::SOUTH_EAST, Direction::SOUTH_WEST, Direction::NORTH_WEST};
+
+		return accumulate(diagonals.begin(), diagonals.end(), 0,
+		                  [&grid, &x, &y](int acc, Direction dir) { return acc + (grid.move(x, y, dir, 1) == 'M' && grid.move(x, y, dir, -1) == 'S'); });
+	};
+
+	for (int y = 1; y < grid.height() - 1; y++)
+	{
+		for (int x = 1; x < grid.width() - 1; x++)
 		{
-			if (grid(x + 1, y + 1) != 'A') continue;
-
-			int crossCount = 0;
-
-      if (grid(x, y) == 'M' && grid(x + 2, y + 2) == 'S') crossCount++;
-      if (grid(x, y) == 'S' && grid(x + 2, y + 2) == 'M') crossCount++;
-      if (grid(x + 2, y) == 'M' && grid(x, y + 2) == 'S') crossCount++;
-      if (grid(x + 2, y) == 'S' && grid(x, y + 2) == 'M') crossCount++;
-
-			if (crossCount >= 2) count++;
+			if (grid(x, y) != 'A') continue;
+			if (crossCount(x, y) >= 2) count++;
 		}
 	}
 
