21044eaa52191acbb7c495560d7ff86388a65ee8
Liam Metzger <liam.metzer@bluewin.ch>
2024-12-06 15:48:31 +0100

    Update 2024-06 to use ThreadPool

--- src/2024/Day06.cpp
+++ src/2024/Day06.cpp
@@ -1,9 +1,15 @@
 #include "2024/Day06.hpp"
 
+#include <cstdio>
+#include <future>
+#include <numeric>
 #include <set>
+#include <thread>
 #include <tuple>
 #include <utility>
+#include <vector>
 
+#include "coll-aoc-runner/parallelism/ThreadPool.hpp"
 #include "coll-aoc-runner/structs/Direction.hpp"
 #include "coll-aoc-runner/structs/Grid.hpp"
 
@@ -25,6 +31,23 @@ Day06_2024::Day06_2024()
 	    "......#...";
 }
 
+Direction turnRight(Direction dir)
+{
+	switch (dir)
+	{
+		case Direction::NORTH:
+			return Direction::EAST;
+		case Direction::EAST:
+			return Direction::SOUTH;
+		case Direction::SOUTH:
+			return Direction::WEST;
+		case Direction::WEST:
+			return Direction::NORTH;
+		default:
+			return dir;
+	}
+}
+
 string Day06_2024::part1(const string& input, bool example)
 {
 	set<pair<int, int>> visited{};
@@ -43,39 +66,22 @@ string Day06_2024::part1(const string& input, bool example)
 			}
 		}
 	}
+
 solve:
-
-	auto turnRight = [](Direction dir)
-	{
-		switch (dir)
-		{
-			case Direction::NORTH:
-				return Direction::EAST;
-			case Direction::EAST:
-				return Direction::SOUTH;
-			case Direction::SOUTH:
-				return Direction::WEST;
-			case Direction::WEST:
-				return Direction::NORTH;
-      default:
-        return static_cast<Direction::Value>(dir);
-		}
-	};
-
-	visited.insert(position);
 	try
 	{
 		for (;;)
 		{
-			if (grid.move(position.first, position.second, dir) != '#')
+			visited.insert(position);
+			int magnitude = 0;
+			while (grid.move(position.first, position.second, dir, magnitude + 1) != '#')
 			{
-				position = {position.first + dir.direction().first, position.second + dir.direction().second};
-				visited.insert(position);
+				magnitude++;
+				visited.insert({position.first + dir.direction().first * magnitude, position.second + dir.direction().second * magnitude});
 			}
-			else
-			{
-				dir = turnRight(dir);
-			}
+
+			position = {position.first + dir.direction().first * magnitude, position.second + dir.direction().second * magnitude};
+			dir = turnRight(dir);
 		}
 	}
 	catch (const exception& e)
@@ -87,12 +93,9 @@ solve:
 
 string Day06_2024::part2(const string& input, bool example)
 {
-	set<pair<int, int>> positions{};
-  set<tuple<int, int, Direction>> visited{};
 	pair<int, int> position{0, 0};
 	Grid<char> grid{input};
 	Direction dir = Direction::NORTH;
-
 	for (int y = 0; y < grid.height(); y++)
 	{
 		for (int x = 0; x < grid.width(); x++)
@@ -104,82 +107,66 @@ string Day06_2024::part2(const string& input, bool example)
 			}
 		}
 	}
+
 solve:
+	caoc::parallelism::ThreadPool pool;
+	std::vector<std::future<bool>> loopSearches{};
+	set<pair<int, int>> visited{position};
 
-	auto turnRight = [](Direction dir)
-	{
-		switch (dir)
-		{
-			case Direction::NORTH:
-				return Direction::EAST;
-			case Direction::EAST:
-				return Direction::SOUTH;
-			case Direction::SOUTH:
-				return Direction::WEST;
-			case Direction::WEST:
-				return Direction::NORTH;
-      default:
-        return static_cast<Direction::Value>(dir);
-		}
-	};
-	auto isLoop = [&turnRight](Grid<char>& grid, pair<int, int> pos, auto& isLoop, Direction dir = Direction::NORTH, set<tuple<int, int, Direction>> visited = {}) -> bool
-	{
-		if (visited.find({pos.first, pos.second, dir}) != visited.end())
-		{
-			return true;
-		}
-		visited.insert({pos.first, pos.second, dir});
-		try
-		{
-      int magnitude = 0;
-      while (grid.move(pos.first, pos.second, dir, magnitude + 1) != '#') magnitude++;
-
-      pos = {pos.first + dir.direction().first * magnitude, pos.second + dir.direction().second * magnitude};
-      dir = turnRight(dir);
-		}
-		catch (const exception& e)
-		{
-			return false;
-		}
-		return isLoop(grid, pos, isLoop, dir, visited);
-	};
-
-	auto pathPos = position;
-	long long loops = 0;
-  int i = 0;
 	try
 	{
 		for (;;)
 		{
-			if (grid.move(pathPos.first, pathPos.second, dir) != '#')
+			int magnitude = 0;
+			while (grid.move(position.first, position.second, dir, magnitude + 1) != '#')
 			{
-				pathPos = {pathPos.first + dir.direction().first, pathPos.second + dir.direction().second};
+        magnitude++;
+				if (visited.find({position.first + dir.direction().first * magnitude, position.second + dir.direction().second * magnitude}) == visited.end())
+				{
+          visited.insert({position.first + dir.direction().first * magnitude, position.second + dir.direction().second * magnitude});
+					loopSearches.emplace_back(pool.enqueue(
+					    [](const Grid<char> grid, const pair<int, int> position, int magnitude, const Direction dir) -> bool
+					    {
+						    auto positionCopy = position;
+                Direction dirCopy = dir;
 
-        if (positions.find(pathPos) == positions.end())
-        {
-          auto changedGrid = grid;
-          pair<int, int> changedPos = {pathPos.first - dir.direction().first, pathPos.second - dir.direction().second};
-          auto changeVisited = visited;
-          changedGrid.set(pathPos.first, pathPos.second, '#');
+						    auto gridCopy = grid;
+						    gridCopy.set(position.first + dir.direction().first * magnitude, position.second + dir.direction().second * magnitude, '#');
 
-          if (isLoop(changedGrid, changedPos, isLoop, dir))
-          {
-            loops++;
-          }
-        }
+						    set<tuple<int, int, Direction>> visited{};
+						    try
+						    {
+							    for (;;)
+							    {
+								    if (visited.find({positionCopy.first, positionCopy.second, dirCopy}) != visited.end())
+								    {
+									    return true;
+								    }
+								    visited.insert({positionCopy.first, positionCopy.second, dirCopy});
 
-				positions.insert(pathPos);
-        visited.insert({pathPos.first, pathPos.second, dir});
+								    int magnitudeCopy = 0;
+								    while (gridCopy.move(positionCopy.first, positionCopy.second, dirCopy, magnitudeCopy + 1) != '#') magnitudeCopy++;
+								    positionCopy = {positionCopy.first + dirCopy.direction().first * magnitudeCopy,
+								                    positionCopy.second + dirCopy.direction().second * magnitudeCopy};
+								    dirCopy = turnRight(dirCopy);
+							    }
+						    }
+						    catch (const exception& e)
+						    {
+							    return false;
+						    }
+					    },
+					    grid, position, magnitude, dir));
+				}
 			}
-			else
-			{
-				dir = turnRight(dir);
-			}
+
+			position = {position.first + dir.direction().first * magnitude, position.second + dir.direction().second * magnitude};
+			dir = turnRight(dir);
 		}
 	}
 	catch (const exception& e)
 	{
 	}
 
-	return to_string(loops);
+	return to_string(accumulate(loopSearches.begin(), loopSearches.end(), 0, [](int acc, auto& search) { return acc + search.get(); }));
 }
