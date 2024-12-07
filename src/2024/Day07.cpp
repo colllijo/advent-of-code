c46f8082d0eda0a00743e6e84a427f76c19da00e
Liam Metzger <liam.metzger@bluewin.ch>
2024-12-07 08:57:19 +0100

    Optimize 2024-07

--- src/2024/Day07.cpp
+++ src/2024/Day07.cpp
@@ -1,6 +1,9 @@
 #include "2024/Day07.hpp"
+#include <future>
+#include <numeric>
 #include <sstream>
 #include <vector>
+#include "coll-aoc-runner/parallelism/ThreadPool.hpp"
 
 Day07_2024::Day07_2024() {
 	exampleInput =
@@ -16,100 +19,112 @@ Day07_2024::Day07_2024() {
 }
 
 string Day07_2024::part1(const string& input, bool example) {
-	long long sum = 0;
+	vector<future<long long>> sums;
+	caoc::parallelism::ThreadPool pool;
 
 	stringstream stream(input); string line;
 	while (getline(stream, line)) {
-		long test;
-		vector<long> numbs;
+		sums.emplace_back(pool.enqueue([](string line){
+			long long test;
+			vector<long> numbs;
 
-		test = stol(line.substr(0, line.find(":")));
-		string num;
-		stringstream ss(line.substr(line.find(":") + 2));
+			test = stol(line.substr(0, line.find(":")));
+			string num;
+			stringstream ss(line.substr(line.find(":") + 2));
 
-		while (getline(ss, num, ' ')) {
-			numbs.push_back(stol(num));
-		}
-
-		vector<vector<long>> possibilities = { numbs };
-
-		while (!possibilities.empty()) {
-			vector<long> current = possibilities.back();
-			possibilities.pop_back();
-
-			if (current.size() == 1) {
-				if (current[0] == test) {
-					sum += test;
-					possibilities.clear();
-				}
-				continue;
+			while (getline(ss, num, ' ')) {
+				numbs.push_back(stol(num));
 			}
 
-			long left = current[0];
-			long right = current[1];
+			vector<vector<long>> possibilities = { numbs };
 
-			current.erase(current.begin());
-			vector<long> add = current;
-			vector<long> mul = current;
+			while (!possibilities.empty()) {
+				vector<long> current = possibilities.back();
+				possibilities.pop_back();
 
-			add[0] = left + right;
-			mul[0] = left * right;
+				if (current.size() == 1) {
+					if (current[0] == test) {
+						return test;
+						possibilities.clear();
+					}
+					continue;
+				}
 
-			possibilities.push_back(add);
-			possibilities.push_back(mul);
-		}
+				long left = current[0];
+				long right = current[1];
+
+				current.erase(current.begin());
+
+				vector<long> add = current;
+				vector<long> mul = current;
+
+				add[0] = left + right;
+				mul[0] = left * right;
+
+				if (add[0] <= test) possibilities.push_back(add);
+				if (mul[0] <= test) possibilities.push_back(mul);
+			}
+
+			return 0LL;
+		}, line));
 	}
 
-	return to_string(sum);
+	return to_string(accumulate(sums.begin(), sums.end(), 0LL, [](long long acc, auto& sum) { return acc + sum.get(); }));
 }
 
 string Day07_2024::part2(const string& input, bool example) {
-	long long sum = 0;
+	vector<future<long long>> sums;
+	caoc::parallelism::ThreadPool pool;
 
 	stringstream stream(input); string line;
 	while (getline(stream, line)) {
-		long test;
-		vector<long> numbs;
+		sums.emplace_back(pool.enqueue([](string line){
+			long long test;
+			vector<long> numbs;
 
-		test = stol(line.substr(0, line.find(":")));
-		string num;
-		stringstream ss(line.substr(line.find(":") + 2));
+			test = stol(line.substr(0, line.find(":")));
+			string num;
+			stringstream ss(line.substr(line.find(":") + 2));
 
-		while (getline(ss, num, ' ')) {
-			numbs.push_back(stol(num));
-		}
-
-		vector<vector<long>> possibilities = { numbs };
-
-		while (!possibilities.empty()) {
-			vector<long> current = possibilities.back();
-			possibilities.pop_back();
-
-			if (current.size() == 1) {
-				if (current[0] == test) {
-					sum += test;
-					possibilities.clear();
-				}
-				continue;
+			while (getline(ss, num, ' ')) {
+				numbs.push_back(stol(num));
 			}
 
-			long left = current[0];
-			long right = current[1];
+			vector<vector<long>> possibilities = { numbs };
 
-			current.erase(current.begin());
-			vector<long> add = current;
-			vector<long> mul = current;
-			vector<long> concat = current;
+			while (!possibilities.empty()) {
+				vector<long> current = possibilities.back();
+				possibilities.pop_back();
 
-			add[0] = left + right;
-			mul[0] = left * right;
-			concat[0] = stol(string(to_string(left)  + to_string(right)));
+				if (current.size() == 1) {
+					if (current[0] == test) {
+						return test;
+						possibilities.clear();
+					}
+					continue;
+				}
 
-			possibilities.push_back(add);
-			possibilities.push_back(mul);
-			possibilities.push_back(concat);
-		}
+				long left = current[0];
+				long right = current[1];
+
+				current.erase(current.begin());
+				vector<long> add = current;
+				vector<long> mul = current;
+				vector<long> concat = current;
+
+
+				add[0] = left + right;
+				mul[0] = left * right;
+				concat[0] = stol(string(to_string(left)  + to_string(right)));
+
+				if (add[0] <= test) possibilities.push_back(add);
+				if (mul[0] <= test) possibilities.push_back(mul);
+				if (concat[0] <= test) possibilities.push_back(concat);
+			}
+
+			return 0LL;
+		}, line));
 	}
 
-	return to_string(sum);
+	return to_string(accumulate(sums.begin(), sums.end(), 0LL, [](long long acc, auto& sum) { return acc + sum.get(); }));
 }
