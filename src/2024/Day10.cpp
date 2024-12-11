b346f42553886000386a507fa3658127e8294489
Liam Metzger <liam.metzger@bluewin.ch>
2024-12-11 06:42:32 +0100

    Solve 2024-10

--- src/2024/Day10.cpp
+++ src/2024/Day10.cpp
@@ -1,13 +1,123 @@
 #include "2024/Day10.hpp"
 
-Day10_2024::Day10_2024() {
-	exampleInput = "";
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "coll-aoc-runner/structs/Direction.hpp"
+#include "coll-aoc-runner/structs/Grid.hpp"
+
+Day10_2024::Day10_2024()
+{
+	exampleInput =
+	    "89010123\n"
+	    "78121874\n"
+	    "87430965\n"
+	    "96549874\n"
+	    "45678903\n"
+	    "32019012\n"
+	    "01329801\n"
+	    "10456732";
 }
 
-string Day10_2024::part1(const string& input, bool example) {
-	return "TODO: Solve part 1.";
+const vector<caoc::structs::Direction> dirs = {
+    caoc::structs::Direction::NORTH,
+    caoc::structs::Direction::SOUTH,
+    caoc::structs::Direction::WEST,
+    caoc::structs::Direction::EAST,
+};
+
+string Day10_2024::part1(const string& input, bool example)
+{
+	caoc::structs::Grid<int> grid(input, [](char c) { return c - '0'; });
+	vector<pair<int, int>> pos;
+
+	for (int y = 0; y < grid.height(); y++)
+	{
+		for (int x = 0; x < grid.width(); x++)
+		{
+			if (grid.get(x, y) == 0)
+			{
+				pos.push_back({x, y});
+			}
+		}
+	}
+	long long sum = 0;
+	for (const auto& p : pos)
+	{
+		set<pair<int, int>> paths {p};
+
+		int step = 1;
+		while (step <= 9)
+		{
+			set<pair<int, int>> newPaths;
+			for (const auto& [x, y] : paths)
+			{
+				for (const auto& dir : dirs)
+				{
+					int nx = x + dir.direction().first;
+					int ny = y + dir.direction().second;
+					if (grid.inBounds(nx, ny) && grid.get(nx, ny) == step)
+					{
+						newPaths.insert({nx, ny});
+					}
+				}
+			}
+
+			paths = newPaths;
+			step++;
+		}
+
+		sum += paths.size();
+	}
+
+	return to_string(sum);
 }
 
-string Day10_2024::part2(const string& input, bool example) {
-	return "TODO: Solve part 2.";
+string Day10_2024::part2(const string& input, bool example)
+{
+	caoc::structs::Grid<int> grid(input, [](char c) { return c - '0'; });
+	vector<pair<int, int>> pos;
+
+	for (int y = 0; y < grid.height(); y++)
+	{
+		for (int x = 0; x < grid.width(); x++)
+		{
+			if (grid.get(x, y) == 0)
+			{
+				pos.push_back({x, y});
+			}
+		}
+	}
+	long long sum = 0;
+	for (const auto& p : pos)
+	{
+		vector<pair<int, int>> paths {p};
+
+		int step = 1;
+		while (step <= 9)
+		{
+			vector<pair<int, int>> newPaths;
+			for (const auto& [x, y] : paths)
+			{
+				for (const auto& dir : dirs)
+				{
+					int nx = x + dir.direction().first;
+					int ny = y + dir.direction().second;
+					if (grid.inBounds(nx, ny) && grid.get(nx, ny) == step)
+					{
+						newPaths.push_back({nx, ny});
+					}
+				}
+			}
+
+			paths = newPaths;
+			step++;
+		}
+
+		sum += paths.size();
+	}
+
+	return to_string(sum);
 }
