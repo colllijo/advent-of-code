6bd60d7f0a5d07dc38356b0746b11fd3d249e2ba
Liam Metzger <liam.metzer@bluewin.ch>
2024-12-11 08:31:33 +0100

    Solve 2024-11 use lookup to make time more feasible

--- src/2024/Day11.cpp
+++ src/2024/Day11.cpp
@@ -1,107 +1,96 @@
 #include "2024/Day11.hpp"
 
 #include <future>
+#include <mutex>
 #include <numeric>
 #include <sstream>
+#include <unordered_map>
+#include <utility>
 #include <vector>
+
 #include "coll-aoc-runner/parallelism/ThreadPool.hpp"
 
 Day11_2024::Day11_2024() { exampleInput = "125 17"; }
 
+template <typename a, typename b>
+struct hash<std::pair<a, b>>
+{
+private:
+	const hash<a> ah;
+	const hash<b> bh;
+
+public:
+	hash() : ah(), bh() {}
+	size_t operator()(const std::pair<a, b>& p) const { return ah(p.first) ^ bh(p.second); }
+};
+
+unsigned long long getRockCount(long long stone, int blinks, unordered_map<pair<long long, int>, unsigned long long>& lookup, mutex& lookupMutex)
+{
+	if (blinks == 0) return 1;
+
+  {
+    lock_guard<mutex> lock(lookupMutex);
+    if (lookup.find({stone, blinks}) != lookup.end())
+    {
+      return lookup[{stone, blinks}];
+    }
+  }
+
+  auto ret = [&lookup, &lookupMutex, &stone, &blinks](unsigned long long val){
+      lock_guard<mutex> lock(lookupMutex);
+      lookup[{stone, blinks}] = val;
+      return val;
+  };
+
+	if (stone == 0)
+	{
+		return ret(getRockCount(1, blinks - 1, lookup, lookupMutex));
+	}
+	if (to_string(stone).size() & 0b1)
+	{
+		return ret(getRockCount(stone * 2024, blinks - 1, lookup, lookupMutex));
+	}
+	else
+	{
+		long long left = stoll(to_string(stone).substr(0, to_string(stone).size() / 2));
+		long long right = stoll(to_string(stone).substr(to_string(stone).size() / 2));
+
+		return ret(getRockCount(left, blinks - 1, lookup, lookupMutex) + getRockCount(right, blinks - 1, lookup, lookupMutex));
+	}
+}
+
 string Day11_2024::part1(const string& input, bool example)
 {
 	caoc::parallelism::ThreadPool pool;
-	vector<future<size_t>> futureStones;
+	vector<future<unsigned long long>> futureStones;
 	vector<long long> stones;
+
+	unordered_map<pair<long long, int>, unsigned long long> lookup;
+	mutex lookupMutex;
 
 	stringstream stream(input);
 	string line;
-	while (getline(stream, line, ' '))
-	{
-		stones.push_back(stoll(line));
-	}
+	while (getline(stream, line, ' ')) stones.push_back(stoll(line));
 
-	for (const auto& stone : stones)
-	{
-		futureStones.push_back(pool.enqueue([](long long stone) {
-			vector<long long> stones{ stone };
+	for (const auto& stone : stones) futureStones.push_back(pool.enqueue([&lookup, &lookupMutex](long long stone) { return getRockCount(stone, 25, lookup, lookupMutex); }, stone));
 
-			for (int blinks = 0; blinks < 25; blinks++)
-			{
-				for (size_t i = 0; i < stones.size(); i++)
-				{
-					if (stones[i] == 0)
-					{
-						stones[i] = 1;
-					}
-					else if ((to_string(stones[i]).size() & 0b1) == 0)
-					{
-						long long left = stoll(to_string(stones[i]).substr(0, to_string(stones[i]).size() / 2));
-						long long right = stoll(to_string(stones[i]).substr(to_string(stones[i]).size() / 2));
-
-						stones[i] = left;
-						stones.insert(stones.begin() + i + 1, right);
-						i++;
-					}
-					else
-					{
-						stones[i] *= 2024;
-					}
-				}
-			}
-
-			return stones.size();
-		}, stone));
-	}
-
-	return to_string(accumulate(futureStones.begin(), futureStones.end(), 0, [](size_t sum, future<size_t>& f) { return sum + f.get(); }));
+	return to_string(accumulate(futureStones.begin(), futureStones.end(), 0, [](unsigned long long sum, future<unsigned long long>& f) { return sum + f.get(); }));
 }
 
 string Day11_2024::part2(const string& input, bool example)
 {
 	caoc::parallelism::ThreadPool pool;
-	vector<future<size_t>> futureStones;
+	vector<future<unsigned long long>> futureStones;
 	vector<long long> stones;
+
+	unordered_map<pair<long long, int>, unsigned long long> lookup;
+	mutex lookupMutex;
 
 	stringstream stream(input);
 	string line;
-	while (getline(stream, line, ' '))
-	{
-		stones.push_back(stoll(line));
-	}
+	while (getline(stream, line, ' ')) stones.push_back(stoll(line));
 
-	for (const auto& stone : stones)
-	{
-		futureStones.push_back(pool.enqueue([](long long stone) {
-			vector<long long> stones{ stone };
+	for (const auto& stone : stones) futureStones.push_back(pool.enqueue([&lookup, &lookupMutex](long long stone) { return getRockCount(stone, 75, lookup, lookupMutex); }, stone));
 
-			for (int blinks = 0; blinks < 75; blinks++)
-			{
-				for (size_t i = 0; i < stones.size(); i++)
-				{
-					if (stones[i] == 0)
-					{
-						stones[i] = 1;
-					}
-					else if ((to_string(stones[i]).size() & 0b1) == 0)
-					{
-						long long left = stoll(to_string(stones[i]).substr(0, to_string(stones[i]).size() / 2));
-						long long right = stoll(to_string(stones[i]).substr(to_string(stones[i]).size() / 2));
-
-						stones[i] = left;
-						stones.insert(stones.begin() + i + 1, right);
-						i++;
-					}
-					else
-					{
-						stones[i] *= 2024;
-					}
-				}
-			}
-
-			return stones.size();
-		}, stone));
-	}
-
-	return to_string(accumulate(futureStones.begin(), futureStones.end(), 0, [](size_t sum, future<size_t>& f) { return sum + f.get(); }));
+	return to_string(accumulate(futureStones.begin(), futureStones.end(), 0ULL, [](unsigned long long sum, future<unsigned long long>& f) { return sum + f.get(); }));
 }
