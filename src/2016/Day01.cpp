0bc58dca929bbe69660d36d638e9915285e6b3d6
Liam Metzger <liam.metzer@bluewin.ch>
2024-12-16 16:03:30 +0100

    Use new Direction

--- src/2016/Day01.cpp
+++ src/2016/Day01.cpp
@@ -1,22 +1,12 @@
 #include "2016/Day01.hpp"
-
-#include <set>
-#include <string>
+#include "coll-aoc-runner/structs/Vector2.hpp"
 
 Day01_2016::Day01_2016() { exampleInput = "R8, R4, R4, R8"; }
 
-enum class Direction
-{
-	North,
-	West,
-	South,
-	East
-};
-
 string Day01_2016::part1(const string& input, bool example)
 {
-	pair<int, int> position = {0, 0};
-	Direction facing = Direction::North;
+	Vector2<int> position = {0, 0};
+	Direction facing = Direction::NORTH;
 
 	size_t prevpos = 0, pos = input.find(',');
 	for (;;)
@@ -24,22 +14,12 @@ string Day01_2016::part1(const string& input, bool example)
 		char turn = input[prevpos];
 		int steps = stoi(input.substr(prevpos + 1, pos - prevpos - 1));
 
-		facing = static_cast<Direction>((static_cast<int>(facing) + (turn == 'L' ? 1 : -1) + 4) % 4);
-		switch (facing)
-		{
-			case Direction::North:
-				position.second += steps;
-				break;
-			case Direction::South:
-				position.second -= steps;
-				break;
-			case Direction::East:
-				position.first += steps;
-				break;
-			case Direction::West:
-				position.first -= steps;
-				break;
-		}
+    if (turn == 'L')
+      facing.rotateLeft();
+    else
+      facing.rotateRight();
+
+    position += facing.direction() * steps;
 
 		if (pos == string::npos) break;
 
@@ -47,17 +27,17 @@ string Day01_2016::part1(const string& input, bool example)
 		pos = input.find(',', prevpos);
 	}
 
-	int distance = abs(position.first) + abs(position.second);
+	int distance = abs(position.x()) + abs(position.y());
 
 	return to_string(distance);
 }
 
 string Day01_2016::part2(const string& input, bool example)
 {
-	set<pair<int, int>> visited;
+	unordered_set<Vector2<int>> visited;
 
-	pair<int, int> position = {0, 0};
-	Direction facing = Direction::North;
+	Vector2<int> position = {0, 0};
+	Direction facing = Direction::NORTH;
 	int distance = -1;
 
 	size_t prevpos = 0, pos = input.find(',');
@@ -65,28 +45,15 @@ string Day01_2016::part2(const string& input, bool example)
 	{
 		char turn = input[prevpos];
 		int steps = stoi(input.substr(prevpos + 1, pos - prevpos - 1));
+
     auto move = [&position, example, &facing, &visited](int steps, auto& move) -> int {
       if (steps == 0) return -1;
 
-      switch (facing)
-      {
-        case Direction::North:
-          position.second++;
-          break;
-        case Direction::South:
-          position.second--;
-          break;
-        case Direction::East:
-          position.first++;
-          break;
-        case Direction::West:
-          position.first--;
-          break;
-      }
+      position += facing.direction();
 
       if (visited.contains(position))
       {
-        return abs(position.first) + abs(position.second);
+        return abs(position.x()) + abs(position.y());
       }
       else
       {
@@ -95,7 +62,11 @@ string Day01_2016::part2(const string& input, bool example)
       }
     };
 
-		facing = static_cast<Direction>((static_cast<int>(facing) + (turn == 'L' ? 1 : -1) + 4) % 4);
+    if (turn == 'L')
+      facing.rotateLeft();
+    else
+      facing.rotateRight();
+
     distance = move(steps, move);
 
 		if (distance != -1 || pos == string::npos) break;
