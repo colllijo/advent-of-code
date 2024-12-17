c0c5193663aa0449521e5ec6a739173c58a8fed4
Liam Metzger <liam.metzger@bluewin.ch>
2024-12-17 20:13:00 +0100

    Dijkstra's_algorithm for da speed

--- src/2024/Day16.cpp
+++ src/2024/Day16.cpp
@@ -1,17 +1,8 @@
 #include "2024/Day16.hpp"
 
-#include <algorithm>
 #include <climits>
-#include <map>
-#include <set>
-#include <tuple>
-#include <utility>
-#include <vector>
-
-#include "coll-aoc-runner/structs/Grid.hpp"
-#include "coll-aoc-runner/structs/Vector2.hpp"
-
-using namespace caoc::structs;
+#include <functional>
+#include <queue>
 
 Day16_2024::Day16_2024()
 {
@@ -39,6 +30,7 @@ string Day16_2024::part1(const string& input, bool example)
 
 	Vector2<int> start;
 	Vector2<int> end;
+	Direction startDir = Direction::EAST;
 
 	for (int y = 0; y < grid.height(); y++)
 	{
@@ -55,43 +47,68 @@ string Day16_2024::part1(const string& input, bool example)
 		}
 	}
 
-	vector<long long> costs;
-	map<pair<Vector2<int>, Direction>, long long> visited;
-	set<tuple<Vector2<int>, Direction, long long>> frontline;
+	using Pos = tuple<Vector2<int>, Direction>;
+	using Node = tuple<uint64_t, Vector2<int>, Direction>;
+	priority_queue<Node, vector<Node>, greater<Node>> queue;
 
-	frontline.insert(make_tuple(start, Direction::EAST, 0));
-	while (!frontline.empty())
+	map<Pos, u_int64_t> dist;
+	map<Pos, optional<Pos>> prev;
+
+	dist[{start, startDir}] = 0;
+	prev[{start, startDir}] = nullopt;
+
+	queue.push({0, start, startDir});
+
+	for (int y = 0; y < grid.height(); y++)
 	{
-		set<tuple<Vector2<int>, Direction, long long>> advances{};
-
-		for (const auto& [position, dir, cost] : frontline)
+		for (int x = 0; x < grid.width(); x++)
 		{
-			if (visited.find({position, dir}) != visited.end() && visited[{position, dir}] <= cost)
+			if (grid.get(x, y) != '#' && start != Vector2<int>{x, y})
 			{
-				continue;
+				for (const auto& dir : CardinalDirections)
+				{
+					dist[{{x, y}, dir}] = UINT64_MAX;
+					prev[{{x, y}, dir}] = nullopt;
+					queue.push({UINT64_MAX, {x, y}, dir});
+				}
 			}
-			visited[{position, dir}] = cost;
-
-			if (position == end)
-			{
-				costs.push_back(cost);
-				continue;
-			}
-
-			if (grid.move(position, dir) != '#')
-			{
-				advances.insert({position + dir.direction(), dir, cost + 1});
-			}
-			advances.insert({position, Direction::rotateLeft(dir), cost + 1000});
-			advances.insert({position, Direction::rotateRight(dir), cost + 1000});
 		}
-
-		frontline = advances;
 	}
 
-	sort(costs.begin(), costs.end());
+	while (!queue.empty())
+	{
+		auto [cost, pos, dir] = queue.top();
+		queue.pop();
 
-	return to_string(costs[0]);
+		if (pos == end)
+			return to_string(cost);
+
+		if (grid.move(pos, dir) != '#')
+		{
+			if (cost + 1 < dist[{pos + dir.direction(), dir}])
+			{
+				prev[{pos + dir.direction(), dir}] = {pos, dir};
+				dist[{pos + dir.direction(), dir}] = cost + 1;
+				queue.push({cost + 1, pos + dir.direction(), dir});
+			}
+		}
+
+		if (cost + 1000 < dist[{pos, Direction::rotateLeft(dir)}])
+		{
+			prev[{pos, Direction::rotateLeft(dir)}] = {pos, dir};
+			dist[{pos, Direction::rotateLeft(dir)}] = cost + 1000;
+			queue.push({cost + 1000, pos, Direction::rotateLeft(dir)});
+		}
+
+		if (cost + 1000 < dist[{pos, Direction::rotateRight(dir)}])
+		{
+			prev[{pos, Direction::rotateRight(dir)}] = {pos, dir};
+			dist[{pos, Direction::rotateRight(dir)}] = cost + 1000;
+			queue.push({cost + 1000, pos, Direction::rotateRight(dir)});
+		}
+	}
+
+	return "End was not found";
 }
 
 string Day16_2024::part2(const string& input, bool example)
@@ -100,6 +117,7 @@ string Day16_2024::part2(const string& input, bool example)
 
 	Vector2<int> start;
 	Vector2<int> end;
+	Direction startDir = Direction::EAST;
 
 	for (int y = 0; y < grid.height(); y++)
 	{
@@ -116,53 +134,98 @@ string Day16_2024::part2(const string& input, bool example)
 		}
 	}
 
-	long long minCost = LLONG_MAX;
-	map<pair<Vector2<int>, Direction>, long long> visited;
-	set<tuple<Vector2<int>, Direction, long long, set<Vector2<int>>>> frontline;
-	set<Vector2<int>> paths;
+	using Pos = tuple<Vector2<int>, Direction>;
+	using Node = tuple<uint64_t, Vector2<int>, Direction>;
+	priority_queue<Node, vector<Node>, greater<Node>> queue;
 
-	frontline.insert(make_tuple(start, Direction::EAST, 0, set<Vector2<int>>{}));
-	while (!frontline.empty())
+	map<Pos, u_int64_t> dist;
+	map<Pos, vector<Pos>> prev;
+
+	dist[{start, startDir}] = 0;
+	prev[{start, startDir}] = {};
+
+	queue.push({0, start, startDir});
+
+	for (int y = 0; y < grid.height(); y++)
 	{
-		set<tuple<Vector2<int>, Direction, long long, set<Vector2<int>>>> advances{};
-
-		for (const auto& [position, dir, cost, path] : frontline)
+		for (int x = 0; x < grid.width(); x++)
 		{
-			if (visited.find({position, dir}) != visited.end() && visited[{position, dir}] < cost)
+			if (grid.get(x, y) != '#' && start != Vector2<int>{x, y})
 			{
-				continue;
-			}
-			visited[{position, dir}] = cost;
-
-			auto pathCopy = path;
-			pathCopy.insert(position);
-
-			if (position == end)
-			{
-				if (cost < minCost)
+				for (const auto& dir : CardinalDirections)
 				{
-					minCost = cost;
-					paths.clear();
+					dist[{{x, y}, dir}] = UINT64_MAX;
+					prev[{{x, y}, dir}] = {};
+					queue.push({UINT64_MAX, {x, y}, dir});
 				}
-
-				if (cost == minCost)
-				{
-					paths.insert(pathCopy.begin(), pathCopy.end());
-				}
-				continue;
 			}
-
-			if (grid.move(position, dir) != '#')
-			{
-				advances.insert({position + dir.direction(), dir, cost + 1, pathCopy});
-			}
-			advances.insert({position, Direction::rotateLeft(dir), cost + 1000, pathCopy});
-			advances.insert({position, Direction::rotateRight(dir), cost + 1000, pathCopy});
 		}
-
-		frontline = advances;
 	}
 
+	Pos finalPos;
+	while (!queue.empty())
+	{
+		auto [cost, pos, dir] = queue.top();
+		queue.pop();
 
-	return to_string(paths.size());
+		if (pos == end)
+		{
+			finalPos = {pos, dir};
+			break;
+		}
+
+		if (grid.move(pos, dir) != '#')
+		{
+			if (cost + 1 > cost && cost + 1 < dist[{pos + dir.direction(), dir}])
+			{
+				prev[{pos + dir.direction(), dir}] = {{pos, dir}};
+				dist[{pos + dir.direction(), dir}] = cost + 1;
+				queue.push({cost + 1, pos + dir.direction(), dir});
+			}
+			else if (cost + 1 > cost && cost + 1 == dist[{pos + dir.direction(), dir}])
+			{
+				prev[{pos + dir.direction(), dir}].push_back({pos, dir});
+			}
+		}
+
+		if (cost + 1000 > cost && cost + 1000 < dist[{pos, Direction::rotateLeft(dir)}])
+		{
+			prev[{pos, Direction::rotateLeft(dir)}] = {{pos, dir}};
+			dist[{pos, Direction::rotateLeft(dir)}] = cost + 1000;
+			queue.push({cost + 1000, pos, Direction::rotateLeft(dir)});
+		}
+		else if (cost + 1000 > cost && cost + 1000 == dist[{pos, Direction::rotateLeft(dir)}])
+		{
+			prev[{pos, Direction::rotateLeft(dir)}].push_back({pos, dir});
+		}
+
+		if (cost + 1000 > cost && cost + 1000 < dist[{pos, Direction::rotateRight(dir)}])
+		{
+			prev[{pos, Direction::rotateRight(dir)}] = {{pos, dir}};
+			dist[{pos, Direction::rotateRight(dir)}] = cost + 1000;
+			queue.push({cost + 1000, pos, Direction::rotateRight(dir)});
+		}
+		else if (cost + 1000 > cost && cost + 1000 == dist[{pos, Direction::rotateRight(dir)}])
+		{
+			prev[{pos, Direction::rotateRight(dir)}].push_back({pos, dir});
+		}
+	}
+
+	set<Vector2<int>> visited{get<0>(finalPos)};
+	vector<Pos> path = prev[finalPos];
+
+	while(!path.empty())
+	{
+		vector<Pos> steps;
+
+		for (const auto& p : path)
+		{
+			visited.insert(get<0>(p));
+			steps.insert(steps.end(), prev[p].begin(), prev[p].end());
+		}
+
+		path = steps;
+	}
+
+	return to_string(visited.size());
 }
