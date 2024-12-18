f3eab8a454921932cdd99fc269601a8ef04fa1ee
Liam Metzger <liam.metzer@bluewin.ch>
2024-12-18 14:04:01 +0100

    Minimize usage of expensive atan2 function

--- lib/coll-aoc-runner/include/coll-aoc-runner/structs/Vector2.hpp
+++ lib/coll-aoc-runner/include/coll-aoc-runner/structs/Vector2.hpp
@@ -50,29 +50,16 @@ namespace caoc::structs
 	private:
 		T m_x;
 		T m_y;
-
-		T m_magnitude;
-		double m_direction;
 	};
 
 	template <arithmetic T>
-	Vector2<T>::Vector2() : m_x(0), m_y(0), m_magnitude(0), m_direction(0)
+	Vector2<T>::Vector2() : m_x(0), m_y(0)
 	{
 	}
 
 	template <arithmetic T>
 	Vector2<T>::Vector2(T x, T y) : m_x(x), m_y(y)
 	{
-		if (x == 0 && y == 0)
-		{
-			m_magnitude = 0;
-			m_direction = 0;
-		}
-		else
-		{
-			m_magnitude = std::sqrt(x * x + y * y);
-			m_direction = std::atan2(y, x);
-		}
 	}
 
 	template <arithmetic T>
@@ -85,17 +72,6 @@ namespace caoc::structs
 	void Vector2<T>::setX(T x)
 	{
 		m_x = x;
-
-		if (m_y == 0 && x == 0)
-		{
-			m_magnitude = 0;
-			m_direction = 0;
-		}
-		else
-		{
-			m_magnitude = std::sqrt(x * x + m_y * m_y);
-			m_direction = std::atan2(m_y, x);
-		}
 	}
 
 	template <arithmetic T>
@@ -108,59 +84,59 @@ namespace caoc::structs
 	void Vector2<T>::setY(T y)
 	{
 		m_y = y;
-
-		if (m_x == 0 && y == 0)
-		{
-			m_magnitude = 0;
-			m_direction = 0;
-		}
-		else
-		{
-			m_magnitude = std::sqrt(m_x * m_x + y * y);
-			m_direction = std::atan2(y, m_x);
-		}
 	}
 
 	template <arithmetic T>
 	T Vector2<T>::magnitude() const
 	{
-		return m_magnitude;
+		if (m_x == 0 && m_y == 0) return 0;
+
+		return std::sqrt(m_x * m_x + m_y * m_y);
 	}
 
 	template <arithmetic T>
 	void Vector2<T>::setMagnitude(T magnitude)
 	{
-		m_magnitude = magnitude;
-		m_x = m_magnitude * std::cos(m_direction);
-		m_y = m_magnitude * std::sin(m_direction);
+		double direction = this->direction();
+
+		m_x = magnitude * std::cos(direction);
+		m_y = magnitude * std::sin(direction);
 	}
 
 	template <arithmetic T>
 	double Vector2<T>::direction() const
 	{
-		return m_direction;
+		if (m_x == 0 && m_y == 0)
+			return 0;
+		else
+			return std::atan2(m_y, m_x);
 	}
 
 	template <arithmetic T>
 	void Vector2<T>::setDirection(double direction)
 	{
-		m_direction = direction;
-		m_x = m_magnitude * std::cos(m_direction);
-		m_y = m_magnitude * std::sin(m_direction);
+    T magnitude = this->magnitude();
+    
+		m_x = magnitude * std::cos(direction);
+		m_y = magnitude * std::sin(direction);
 	}
 
 	template <arithmetic T>
 	Vector2<T> Vector2<T>::normal() const
 	{
-		return Vector2<T>(m_x / m_magnitude, m_y / m_magnitude);
+    T magnitude = this->magnitude();
+
+		return Vector2<T>(m_x / magnitude, m_y / magnitude);
 	}
 
 	template <arithmetic T>
 	Vector2<T>& Vector2<T>::normalize()
 	{
-		m_x /= m_magnitude;
-		m_y /= m_magnitude;
-		m_magnitude = 1;
+    T magnitude = this->magnitude();
+
+		m_x /= magnitude;
+		m_y /= magnitude;
+
 		return *this;
 	}
 
@@ -203,7 +179,7 @@ namespace caoc::structs
 	template <arithmetic T>
 	Vector2<T>& Vector2<T>::operator*=(T scalar)
 	{
-		setMagnitude(m_magnitude * scalar);
+		setMagnitude(magnitude() * scalar);
 
 		return *this;
 	}
@@ -223,14 +199,11 @@ namespace caoc::structs
 	template <arithmetic T>
 	std::strong_ordering Vector2<T>::operator<=>(const Vector2<T>& other) const
 	{
-		if (auto cmp = m_magnitude <=> other.m_magnitude; cmp != 0)
-			return cmp;
+		if (auto cmp = magnitude() <=> other.magnitude(); cmp != 0) return cmp;
 
-		auto direction_cmp = m_direction <=> other.m_direction;
-		if (direction_cmp == std::partial_ordering::less)
-			return std::strong_ordering::less;
-		if (direction_cmp == std::partial_ordering::greater)
-			return std::strong_ordering::greater;
+		auto direction_cmp = direction() <=> other.direction();
+		if (direction_cmp == std::partial_ordering::less) return std::strong_ordering::less;
+		if (direction_cmp == std::partial_ordering::greater) return std::strong_ordering::greater;
 		return std::strong_ordering::equal;
 	}
 
@@ -248,9 +221,6 @@ namespace std
 	template <arithmetic T>
 	struct hash<caoc::structs::Vector2<T>>
 	{
-		size_t operator()(const caoc::structs::Vector2<T>& v) const
-		{
-			return (hash<T>()(v.magnitude()) ^ (hash<double>()(v.direction()) << 1));
-		}
+		size_t operator()(const caoc::structs::Vector2<T>& v) const { return (hash<T>()(v.magnitude()) ^ (hash<double>()(v.direction()) << 1)); }
 	};
 }  // namespace std
