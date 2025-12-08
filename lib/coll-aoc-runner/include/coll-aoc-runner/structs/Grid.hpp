7b442d07b1197e036e21e1edae92a216c27a300a
Liam Metzger <liam.metzer@bluewin.ch>
2025-12-08 15:09:34 +0100

    Solve 2025-06

--- lib/coll-aoc-runner/include/coll-aoc-runner/structs/Grid.hpp
+++ lib/coll-aoc-runner/include/coll-aoc-runner/structs/Grid.hpp
@@ -7,21 +7,19 @@
 #include <string>
 #include <vector>
 
-#include "coll-aoc-runner/strings/Format.hpp"
 #include "coll-aoc-runner/structs/Direction.hpp"
 #include "coll-aoc-runner/structs/Vector2.hpp"
 
 namespace caoc::structs
 {
-	template <typename T>
-	class Grid
+	template <typename T> class Grid
 	{
 	public:
 		Grid();
 		Grid(int width, int height);
 		Grid(int width, int height, T defaultValue);
 		Grid(std::vector<std::vector<T>>);
-		Grid(const std::string& gridRepresentation, const std::function<T(char)>& transform = [](char c) { return c; });
+		Grid(const std::string &gridRepresentation, const std::function<T(char)> &transform = [](char c) { return c; });
 
 		int width() const;
 		int height() const;
@@ -44,10 +42,10 @@ namespace caoc::structs
 		std::vector<std::vector<T>> sinisterDiagonals() const;
 		std::vector<std::vector<T>> diagonals() const;
 
-    void print() const;
+		void print() const;
 
-		T& operator()(int x, int y);
-		const T& operator()(int x, int y) const;
+		T &operator()(int x, int y);
+		const T &operator()(int x, int y) const;
 
 	private:
 		int m_width;
@@ -56,71 +54,50 @@ namespace caoc::structs
 		std::vector<std::vector<T>> m_grid;
 	};
 
-	template <typename T>
-	Grid<T>::Grid() : m_width(0), m_height(0)
-	{
-	}
+	template <typename T> Grid<T>::Grid() : m_width(0), m_height(0) {}
 
-	template <typename T>
-	Grid<T>::Grid(int width, int height) : m_width(width), m_height(height), m_grid(height, std::vector<T>(width))
-	{
-	}
+	template <typename T> Grid<T>::Grid(int width, int height) : m_width(width), m_height(height), m_grid(height, std::vector<T>(width)) {}
 
-	template <typename T>
-	Grid<T>::Grid(int width, int height, T defaultValue) : m_width(width), m_height(height), m_grid(height, std::vector<T>(width, defaultValue))
-	{
-	}
+	template <typename T> Grid<T>::Grid(int width, int height, T defaultValue) : m_width(width), m_height(height), m_grid(height, std::vector<T>(width, defaultValue)) {}
 
-	template <typename T>
-	Grid<T>::Grid(std::vector<std::vector<T>> grid) : m_width(grid[0].size()), m_height(grid.size()), m_grid(grid)
-	{
-	}
+	template <typename T> Grid<T>::Grid(std::vector<std::vector<T>> grid) : m_width(grid[0].size()), m_height(grid.size()), m_grid(grid) {}
 
-	template <typename T>
-	Grid<T>::Grid(const std::string& gridRepresentation, const std::function<T(char)>& transform) : m_width(0), m_height(0)
+	template <typename T> Grid<T>::Grid(const std::string &gridRepresentation, const std::function<T(char)> &transform) : m_width(0), m_height(0)
 	{
-		for (const auto& row : caoc::strings::trim(gridRepresentation) | std::ranges::views::split('\n'))
+		for (const auto &row : gridRepresentation | std::views::split('\n') | std::views::filter([](auto &&row) { return !row.empty(); }))
 		{
 			m_grid.push_back(std::vector<T>());
-			for (const auto& cell : row)
-			{
-				m_grid.back().push_back(transform(cell));
-			}
+			for (const auto &cell : row) { m_grid.back().push_back(transform(cell)); }
 			m_width = std::max(m_width, static_cast<int>(m_grid.back().size()));
 		}
 		m_height = m_grid.size();
 	}
 
-	template <typename T>
-	void Grid<T>::set(int x, int y, T value)
+	template <typename T> void Grid<T>::set(int x, int y, T value)
 	{
 		if (y < 0 || y >= m_height || x < 0 || x >= m_width) throw std::out_of_range("Out of bounds");
 
 		m_grid[y][x] = value;
 	}
 
-	template <typename T>
-	void Grid<T>::set(Vector2<int> pos, T value)
+	template <typename T> void Grid<T>::set(Vector2<int> pos, T value)
 	{
 		set(pos.x(), pos.y(), value);
 	}
 
-	template <typename T>
-	T Grid<T>::get(int x, int y) const
+	template <typename T> T Grid<T>::get(int x, int y) const
 	{
 		if (y < 0 || y >= m_height || x < 0 || x >= m_width) throw std::out_of_range("Out of bounds");
 
 		return m_grid[y][x];
 	}
 
-	template <typename T>
-	T Grid<T>::get(Vector2<int> pos) const
+	template <typename T> T Grid<T>::get(Vector2<int> pos) const
 	{
 		return get(pos.x(), pos.y());
 	}
 
-	template <typename T>
-	T Grid<T>::move(int x, int y, Direction direction, int magnitude) const
+	template <typename T> T Grid<T>::move(int x, int y, Direction direction, int magnitude) const
 	{
 		auto scaled = direction.direction() * magnitude;
 
@@ -132,77 +109,61 @@ namespace caoc::structs
 		return m_grid[mY][mX];
 	}
 
-	template <typename T>
-	T Grid<T>::move(Vector2<int> pos, Direction direction, int magnitude) const
+	template <typename T> T Grid<T>::move(Vector2<int> pos, Direction direction, int magnitude) const
 	{
 		return move(pos.x(), pos.y(), direction, magnitude);
 	}
 
-	template <typename T>
-	bool Grid<T>::inBounds(int x, int y) const
+	template <typename T> bool Grid<T>::inBounds(int x, int y) const
 	{
 		return x >= 0 && x < m_width && y >= 0 && y < m_height;
 	}
 
-	template <typename T>
-	bool Grid<T>::inBounds(Vector2<int> pos) const
+	template <typename T> bool Grid<T>::inBounds(Vector2<int> pos) const
 	{
 		return inBounds(pos.x(), pos.y());
 	}
 
-	template <typename T>
-	int Grid<T>::width() const
+	template <typename T> int Grid<T>::width() const
 	{
 		return m_width;
 	}
 
-	template <typename T>
-	int Grid<T>::height() const
+	template <typename T> int Grid<T>::height() const
 	{
 		return m_height;
 	}
 
-	template <typename T>
-	std::vector<T> Grid<T>::row(int y) const
+	template <typename T> std::vector<T> Grid<T>::row(int y) const
 	{
 		if (y < 0 || y >= m_height) throw std::out_of_range("Out of bounds");
 
 		return m_grid[y];
 	}
 
-	template <typename T>
-	std::vector<std::vector<T>> Grid<T>::rows() const
+	template <typename T> std::vector<std::vector<T>> Grid<T>::rows() const
 	{
 		return m_grid;
 	}
 
-	template <typename T>
-	std::vector<T> Grid<T>::column(int x) const
+	template <typename T> std::vector<T> Grid<T>::column(int x) const
 	{
 		if (x < 0 || x >= m_width) throw std::out_of_range("Out of bounds");
 
 		std::vector<T> column;
-		for (int y = 0; y < m_height; y++)
-		{
-			column.push_back(m_grid[y][x]);
-		}
+		for (int y = 0; y < m_height; y++) { column.push_back(m_grid[y][x]); }
 		return column;
 	}
 
-	template <typename T>
-	std::vector<std::vector<T>> Grid<T>::columns() const
+	template <typename T> std::vector<std::vector<T>> Grid<T>::columns() const
 	{
 		std::vector<std::vector<T>> columns;
-		for (int x = 0; x < m_width; x++)
-		{
-			columns.push_back(column(x));
-		}
+		for (int x = 0; x < m_width; x++) { columns.push_back(column(x)); }
 
 		return columns;
 	}
 
-	template <typename T>
-	std::vector<std::vector<T>> Grid<T>::dexterDiagonals() const
+	template <typename T> std::vector<std::vector<T>> Grid<T>::dexterDiagonals() const
 	{
 		std::vector<std::vector<T>> diagonals;
 		for (int i = 0; i < m_height + m_width - 1; i++)
@@ -212,18 +173,14 @@ namespace caoc::structs
 			{
 				int x = i - j;
 				int y = j;
-				if (x < m_width && y < m_height)
-				{
-					diagonal.push_back(m_grid[y][x]);
-				}
+				if (x < m_width && y < m_height) { diagonal.push_back(m_grid[y][x]); }
 			}
 			diagonals.push_back(diagonal);
 		}
 		return diagonals;
 	}
 
-	template <typename T>
-	std::vector<std::vector<T>> Grid<T>::sinisterDiagonals() const
+	template <typename T> std::vector<std::vector<T>> Grid<T>::sinisterDiagonals() const
 	{
 		std::vector<std::vector<T>> diagonals;
 		for (int i = 0; i < m_height + m_width - 1; i++)
@@ -233,20 +190,16 @@ namespace caoc::structs
 			{
 				int x = (m_width - 1) - (i - j);
 				int y = j;
-				if (x >= 0 && y < m_height)
-				{
-					diagonal.push_back(m_grid[y][x]);
-				}
+				if (x >= 0 && y < m_height) { diagonal.push_back(m_grid[y][x]); }
 			}
 			diagonals.push_back(diagonal);
 		}
 		return diagonals;
 	}
 
-	template <typename T>
-	std::vector<std::vector<T>> Grid<T>::diagonals() const
+	template <typename T> std::vector<std::vector<T>> Grid<T>::diagonals() const
 	{
-		auto dexter = dexterDiagonals();
+		auto dexter	  = dexterDiagonals();
 		auto sinister = sinisterDiagonals();
 
 		std::vector<std::vector<T>> diagonals;
@@ -256,32 +209,26 @@ namespace caoc::structs
 		return diagonals;
 	}
 
-  template <typename T>
-  void Grid<T>::print() const
-  {
-    for (const auto& row : m_grid)
-    {
-      for (const auto& cell : row)
-      {
-        std::cout << cell;
-      }
-      std::cout << "\n";
-    }
-  }
+	template <typename T> void Grid<T>::print() const
+	{
+		for (const auto &row : m_grid)
+		{
+			for (const auto &cell : row) { std::cout << cell; }
+			std::cout << "\n";
+		}
+	}
 
-	template <typename T>
-	T& Grid<T>::operator()(int x, int y)
+	template <typename T> T &Grid<T>::operator()(int x, int y)
 	{
 		if (y < 0 || y >= m_height || x < 0 || x >= m_width) throw std::out_of_range("Out of bounds");
 
 		return m_grid[y][x];
 	}
 
-	template <typename T>
-	const T& Grid<T>::operator()(int x, int y) const
+	template <typename T> const T &Grid<T>::operator()(int x, int y) const
 	{
 		if (y < 0 || y >= m_height || x < 0 || x >= m_width) throw std::out_of_range("Out of bounds");
 
 		return m_grid[y][x];
 	}
-};  // namespace caoc::structs
+}; // namespace caoc::structs
--- src/2025/Day06.cpp
+++ src/2025/Day06.cpp
@@ -1,13 +1,77 @@
 #include "2025/Day06.hpp"
 
-Day06_2025::Day06_2025() {
-	exampleInput = "";
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+#include <functional>
+#include <ranges>
+
+#include "coll-aoc-runner/strings/Format.hpp"
+#include "coll-aoc-runner/structs/Grid.hpp"
+
+Day06_2025::Day06_2025()
+{
+	exampleInput = "123 328  51 64 \n"
+				   " 45 64  387 23 \n"
+				   "  6 98  215 314\n"
+				   "*   +   *   +";
 }
 
-string Day06_2025::part1(const string& input, bool example) {
-	return "TODO: Solve part 1.";
+string Day06_2025::part1(const string &input, bool example)
+{
+	std::vector<std::string> lines = caoc::strings::split(input, "\n");
+
+	std::vector<std::string> operations =
+		std::ranges::to<std::vector<std::string>>(lines[lines.size() - 1] | std::views::split(' ') | std::views::filter([](auto &&part) { return !part.empty(); }) |
+												  std::views::transform([](auto &&part) { return std::ranges::to<std::string>(part); }));
+
+	std::vector<std::int64_t> results = std::ranges::to<std::vector<std::int64_t>>(operations | std::views::transform([](auto &&operation) { return operation == "*" ? 1 : 0; }));
+
+	lines.pop_back(); // Remove operations
+	for (const auto &line : lines)
+	{
+		std::vector<std::int64_t> values =
+			std::ranges::to<std::vector<std::int64_t>>(line | std::views::split(' ') | std::views::filter([](auto &&part) { return !part.empty(); }) |
+													   std::views::transform([](auto &&part) { return std::stoll(std::ranges::to<std::string>(part)); }));
+
+		for (std::size_t i = 0; i < results.size(); ++i)
+		{
+			if (operations[i] == "*") { results[i] *= values[i]; }
+			else {
+				results[i] += values[i];
+			}
+		}
+	}
+
+	return std::to_string(std::ranges::fold_left(results, 0, std::plus{}));
 }
 
-string Day06_2025::part2(const string& input, bool example) {
-	return "TODO: Solve part 2.";
+string Day06_2025::part2(const string &input, bool example)
+{
+	caoc::structs::Grid<char> values(input.substr(0, input.find_last_of('\n')));
+
+	std::vector<std::string> operations = std::ranges::to<std::vector<std::string>>(input.substr(input.find_last_of('\n') + 1) | std::views::split(' ') |
+																					std::views::filter([](auto &&part) { return !part.empty(); }) |
+																					std::views::transform([](auto &&part) { return std::ranges::to<std::string>(part); }));
+
+	std::vector<std::int64_t> results = std::ranges::to<std::vector<std::int64_t>>(operations | std::views::transform([](auto &&operation) { return operation == "*" ? 1 : 0; }));
+
+	int opCounter = 0;
+	for (const auto &column : values.columns())
+	{
+		std::string value = std::ranges::to<std::string>(column | std::views::filter([](auto &&chr) { return chr != ' '; }));
+
+		if (value.empty())
+		{
+			opCounter++;
+			continue;
+		}
+
+		if (operations[opCounter] == "*") { results[opCounter] *= std::stoll(value); }
+		else {
+			results[opCounter] += std::stoll(value);
+		}
+	}
+
+	return std::to_string(std::ranges::fold_left(results, 0, std::plus{}));
 }
